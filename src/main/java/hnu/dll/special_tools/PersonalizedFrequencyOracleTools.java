5034b1aa607c6405dd3bffcb3e1fbfb8914d1291
dulei715 <dulei715@126.com>
2025-08-07 18:02:36 +0800

    Saved by Leilei through a script

---
+++ src/main/java/hnu/dll/special_tools/PersonalizedFrequencyOracleTools.java
@@ -0,0 +1,87 @@
+package hnu.dll.special_tools;
+
+import cn.edu.dll.io.print.MyPrint;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PersonalizedFrequencyOracleTools {
+    public static List<Double> getGeneralRandomResponseParameterQ(List<Double> privacyBudgetList, Integer valueDomainSize) {
+        int size = privacyBudgetList.size();
+        List<Double> qList = new ArrayList<>(size);
+        for (Double budget : privacyBudgetList) {
+            qList.add(1.0 / (Math.exp(budget) + valueDomainSize - 1));
+        }
+        return qList;
+    }
+
+    public static List<Double> getGeneralRandomResponseParameterP(List<Double> qList, List<Double> privacyBudgetList) {
+        int size = qList.size();
+        List<Double> pList = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            pList.add(privacyBudgetList.get(i) * qList.get(i));
+        }
+        return pList;
+    }
+
+    public static List<Double> getEstimation(List<Double> obfuscatedCountList, List<Integer> distinctBudgetCount, List<Double> qList, List<Double> pList) {
+        Double paramA = 0D, paramB = 0D;
+        int size = distinctBudgetCount.size();
+        Integer tempG;
+        Double tempQ;
+        int sizeRes = obfuscatedCountList.size();
+        List<Double> result = new ArrayList<>(sizeRes);
+        for (int i = 0; i < size; i++) {
+            tempG = distinctBudgetCount.get(i);
+            tempQ = qList.get(i);
+            paramA += tempG * tempQ;
+            paramB += tempG * (pList.get(i) - tempQ);
+        }
+        for (Double tempFrequency : obfuscatedCountList) {
+            result.add((tempFrequency-paramA) / paramB);
+        }
+        return result;
+    }
+
+    public static Double getPLDPVariance(Integer valueSize, List<Integer> distinctBudgetCountList, List<Double> qList, List<Double> pList, Integer sampleSize) {
+        Double result = 0D, paramA = 0D, paramB = 0D;
+        int size = distinctBudgetCountList.size();
+        Integer tempG;
+        Double tempQ;
+        for (int i = 0; i < size; i++) {
+            tempG = distinctBudgetCountList.get(i);
+            tempQ = qList.get(i);
+            paramA += tempG * (1 - tempQ) * tempQ;
+            paramB += tempG * (pList.get(i) - tempQ);
+        }
+        paramA *= valueSize;
+        paramB = sampleSize * paramB * paramB;
+        return paramA / paramB;
+    }
+
+    public static Double getVariance(List<Double> distinctEpsilonList, List<Integer> distinctBudgetCount, Integer userSize, Integer sampleSize, Integer valueDomainSize) {
+        List<Double> qList = getGeneralRandomResponseParameterQ(distinctEpsilonList, valueDomainSize);
+        List<Double> pList = getGeneralRandomResponseParameterP(qList, distinctEpsilonList);
+        Double sampleVariance = (userSize - sampleSize) * 1.0 / (sampleSize * (userSize - 1));
+        Double pldpVariance = getPLDPVariance(valueDomainSize, distinctBudgetCount, qList, pList, sampleSize);
+        return sampleVariance + pldpVariance;
+    }
+
+    public static void main(String[] args) {
+        List<Double> distinctEpsilonList = Arrays.asList(0.2, 0.4, 0.5, 0.6, 0.8);
+        List<Integer> distinctBudgetCount = Arrays.asList(6, 2, 2, 4, 2);
+        Integer userSize = 16;
+        Integer sampleSize = 2;
+        Integer valueDomainSize = 2;
+        Double variance = getVariance(distinctEpsilonList, distinctBudgetCount, userSize, sampleSize, valueDomainSize);
+        System.out.println(variance);
+        List<Double> obfuscatedList = Arrays.asList(0.5,0.0,0.0,0.5,0.0);
+        List<Double> qList = getGeneralRandomResponseParameterQ(distinctEpsilonList, valueDomainSize);
+        List<Double> pList = getGeneralRandomResponseParameterP(qList, distinctEpsilonList);
+        List<Double> estimationList = getEstimation(obfuscatedList, distinctBudgetCount, qList, pList);
+        MyPrint.showList(estimationList);
+
+    }
+
+}
