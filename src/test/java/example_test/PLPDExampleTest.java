dff2c30ef64e068c7ffbfb306c8fd96129265cc2
dulei715 <dulei715@126.com>
2025-10-02 14:17:24 +0800

    Saved by Leilei through a script

--- src/test/java/example_test/PLPDExampleTest.java
+++ src/test/java/example_test/PLPDExampleTest.java
@@ -1,10 +1,56 @@
 package example_test;
 
+import cn.edu.dll.io.print.MyPrint;
+import cn.edu.dll.struct.BasicPair;
+import hnu.dll.special_tools.PerturbUtils;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PLPDExampleTest {
     @Test
-    public void funA() {
-        List<Double>
+    public void alphaBetaTest() {
+        List<Double> epsilonList = new ArrayList<>();
+        epsilonList.add(0.2);
+        epsilonList.add(0.3);
+        epsilonList.add(0.5);
+        epsilonList.add(0.6);
+        epsilonList.add(0.8);
+
+        Integer domainSize = 2;
+
+        List<Double> pList = new ArrayList<>(), qList = new ArrayList<>();
+        Double tempQ, tempP;
+        for (Double epsilon : epsilonList) {
+            tempQ = 1.0 / (Math.exp(epsilon) + domainSize - 1);
+            tempP = tempQ * Math.exp(epsilon);
+            pList.add(tempP);
+            qList.add(tempQ);
+        }
+
+        Double smallP = pList.get(2), smallQ = qList.get(2);
+        Double largeP = pList.get(3), largeQ = qList.get(3);
+        BasicPair<Double, Double> result = PerturbUtils.getGRRRePerturbParameters(smallP, smallQ, largeP, largeQ, domainSize);
+        System.out.println("p_3 = " + smallP);
+        System.out.println("q_3 = " + smallQ);
+        System.out.println("p_4 = " + largeP);
+        System.out.println("q_4 = " + largeQ);
+        System.out.println("alpha_{3,4} = " + result.getKey());
+        System.out.println("beta_{3,4} = " + result.getValue());
+
+        largeP = pList.get(4);
+        largeQ = qList.get(4);
+
+        MyPrint.showSplitLine("*", 150);
+
+        result = PerturbUtils.getGRRRePerturbParameters(smallP, smallQ, largeP, largeQ, domainSize);
+        System.out.println("p_3 = " + smallP);
+        System.out.println("q_3 = " + smallQ);
+        System.out.println("p_5 = " + largeP);
+        System.out.println("q_5 = " + largeQ);
+        System.out.println("alpha_{3,5} = " + result.getKey());
+        System.out.println("beta_{3,5} = " + result.getValue());
+
     }
 }
