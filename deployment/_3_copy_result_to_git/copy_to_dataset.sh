1b846dd702f0b3bb897272256f610dd403b83ecf
dulei715 <dulei715@126.com>
2025-10-18 22:22:18 +0800

    Saved by Leilei through a script

---
+++ deployment/_3_copy_result_to_git/copy_to_dataset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# add ${gitee_path} to $PATH
+
+cp -rf ${gitee_path}/server_trans/data-trans2325-vldb05/3_2.wEventPLDP/1.main_ldp_result ${dataset_path}/3-2_stream_ldp
+cp -rf ${gitee_path}/server_trans/data-trans2321-vldb01/3_2.wEventPLDP/1.main_ldp_result ${dataset_path}/3-2_stream_ldp
+cp -rf ${gitee_path}/server_trans/data-trans2334-direct01-leileidu/3_2.wEventPLDP/1.main_ldp_result ${dataset_path}/3-2_stream_ldp
+#cp -rf ${gitee_path}/server_trans/data-trans2309-direct02-leileidu/3_2.wEventPLDP/1.main_ldp_result ${dataset_path}/3-2_stream_ldp
+cp -rf ${gitee_path}/server_trans/data-trans2327-direct04-leileidu/3_2.wEventPLDP/1.main_ldp_result ${dataset_path}/3-2_stream_ldp
---
+++ deployment/_3_copy_result_to_git/copy_to_git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# add ${git_file_home} to $PATH
+
+cp -rf ${work4}/1.main_ldp_result ${git_file_home}
